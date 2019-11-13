af34761317ba1f8551ebba4793c6201fe7dda36c
Mauricio Romao <mauricio.mr89@gmail.com>
2019-11-13 16:19:40 -0300

    chore - change endpoint methods

--- index.js
+++ index.js
@@ -2,17 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/project', (req, res) => {
+app.use(express.json());
 
-    return res.json({message:"novo projeto"})
+const projects = [{id:"1",title:"Nome do projeto",tasks:["tarefa 1"]},
+                  {id:"2",title:"Nome do projeto 2",tasks:["tarefa 2"]}]
+
+
+// retorna lista com os projetos
+app.get('/projects', (req, res) => {
+
+    return res.json(projects);
 
 });
 
-
-app.post('/project',(req, res)=>{
-
-    
+// retorna o projeto atravez do id ou index
+// (neste caso será index pois não estou usando o banco de dados)
+app.get('/projects/:id', (req, res) => {
+    const{id} = req.params; 
+    return res.json(projects[id-1]);
 
 });
+
+//inclui um novo projeto
+app.post('/projects',(req, res)=>{
+
+    const body = req.body;
+
+    projects.push(body);
+
+    return res.json(projects);
+});
+
+app.post('/projects/:id/tasks')
+
 
 app.listen(3000);
